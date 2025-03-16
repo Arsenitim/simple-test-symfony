a87e73dfc653ebbd5c2d1ea4ed58d4d8a3708344
Arseni Timofeev <arseni@developitbest.com>
2025-03-16 23:11:11 +0100

    basic tests for the data collector

---
+++ tests/ExtRateApiTest.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\Service\ExtRateApiInterface;
+use App\Service\CoingeckoRateApiService;
+use App\Entity\CurrencyPair;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+
+class ExtRateApiTest extends KernelTestCase
+{
+
+    public function testInterfaceImplementation()
+    {
+        $httpClientMock = $this->createMock(HttpClientInterface::class);
+
+        $extRateApiInterface = new CoingeckoRateApiService($httpClientMock);
+
+        $this->assertInstanceOf(ExtRateApiInterface::class, $extRateApiInterface);
+    }
+
+    public function testFetchingRateData()
+    {
+        // Instantiate a real HttpClientInterface
+        $httpClient = self::getContainer()->get(HttpClientInterface::class);
+
+        $extRateApiInterface = new CoingeckoRateApiService($httpClient);
+
+        $currencyPair = new CurrencyPair();
+        $currencyPair->setCurrencyBase('bitcoin');
+        $currencyPair->setCurrencyQuote('usd');
+
+        $returnedValue = $extRateApiInterface->getCurrentCoinRate($currencyPair);
+
+        // Make sure it's a string...
+        $this->assertIsString($returnedValue);
+
+        // It should be a string with just numbers and a decimal point somewhere in between
+        $this->assertMatchesRegularExpression('/^\d+\.\d+$/', $returnedValue);
+    }
+}
